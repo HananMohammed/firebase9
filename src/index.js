aaa304b8c74d2ae44d7c2ea5f93d7ec3e518315a
HananMohammed <hananmohammed2468@gmail.com>
2022-01-17 01:24:47 +0200

    Setup Firebase

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("HeY ..... ")
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCc3e8BVTAE3BnpaZmt4FLZqbHgwEs3rBI",
+  authDomain: "fir-9-16f43.firebaseapp.com",
+  projectId: "fir-9-16f43",
+  storageBucket: "fir-9-16f43.appspot.com",
+  messagingSenderId: "325187442162",
+  appId: "1:325187442162:web:a76cc1274640500f56076b",
+  measurementId: "G-ZQ58PSTEFV"
+};
+
+initializeApp(firebaseConfig)
